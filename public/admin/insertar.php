a62b390a222eff28c19e398116d991cdd826a0ee
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2022-12-29 22:45:40 +0100

    mejora insertar articulo

--- public/admin/insertar.php
+++ public/admin/insertar.php
@@ -3,21 +3,37 @@ session_start();
 
 require "../../src/auxiliar.php";
 
-$codigo = $_POST['codigo'];
-$descripcion = $_POST['descripcion'];
-$precio = $_POST['precio'];
-$stock = $_POST['stock'];
+// Sanitize user input
+$code = hh($_POST['codigo'], ENT_QUOTES, 'UTF-8');
+$description = hh($_POST['descripcion'], ENT_QUOTES, 'UTF-8');
+$price = filter_input(INPUT_POST, 'precio', FILTER_SANITIZE_NUMBER_FLOAT);
+$stock = filter_input(INPUT_POST, 'stock', FILTER_SANITIZE_NUMBER_INT);
 
-$pdo = conectar();
-$sent = $pdo->prepare("INSERT INTO articulos (codigo, descripcion, precio, stock) VALUES (:codigo, :descripcion, :precio, :stock)");
-$sent->execute([
-    ':codigo' => $codigo,
-    ':descripcion' => $descripcion,
-    ':precio' => $precio,
-    ':stock'  => $stock
-]);
+// Validate user input
+if (!$code || !$description || !$price || !$stock) {
+    $_SESSION['error'] = 'Invalid input. Please try again.';
+    return volver_admin();
+}
 
-$_SESSION['exito'] = 'El artículo se ha añadido correctamente.';
+try {
+    // Connect to database
+    $pdo = conectar();
 
+    // Prepare and execute INSERT statement
+    $stmt = $pdo->prepare("INSERT INTO articulos (codigo, descripcion, precio, stock) VALUES (:code, :description, :price, :stock)");
+    $stmt->execute([
+        ':code' => $code,
+        ':description' => $description,
+        ':price' => $price,
+        ':stock'  => $stock
+    ]);
+
+    // Set success message
+    $_SESSION['success'] = 'The article was added successfully.';
+
+} catch (PDOException $e) {
+    // Set error message
+    $_SESSION['error'] = 'An error occurred while adding the article. Please try again.';
+}
 
 return volver_admin();
