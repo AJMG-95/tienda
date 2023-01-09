d7d3ffa78b67d200816e24364c241e3c95d2256e
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2023-01-09 13:59:25 +0100

    descuento.php arreglado, aplica descuentos en public/index.php

--- public/admin/rebajas.php
+++ public/admin/rebajas.php
@@ -8,19 +8,26 @@ $pdo = conectar();
 
 // Obtener el id del producto y el porcentaje de descuento del usuario
 $id = obtener_post('id');
-$rebaja = obtener_post('rebaja');
+$descuento = obtener_post('rebaja');
 
 // Calcular el nuevo precio del producto
 $sent = $pdo->prepare("SELECT precio FROM articulos WHERE id = :product_id");
-$sent->execute(['product_id' => $product_id]);
+$sent->execute(['product_id' => $id]);
 $linea = $sent->fetch(PDO::FETCH_ASSOC);
 $precio_inicial = $linea['precio'];
-$cantidad_descuento = $precio_inicial * ($porcentaje_descuento / 100);
+$cantidad_descuento = $precio_inicial * ($descuento / 100);
 $nuevo_precio = $precio_inicial - $cantidad_descuento;
 
 // Actualizar el precio del producto en la base de datos
-$sent = $pdo->prepare("UPDATE articulos SET precio = :nuevo_precio WHERE id = :product_id");
-$resultado = $sent->execute(['nuevo_precio' => $nuevo_precio, 'product_id' => $product_id]);
+$sent = $pdo->prepare("UPDATE articulos
+                            SET descuento = :descuento,
+                                cantidad_descuento = :cantidad_descuento
+                            WHERE id = :product_id");
+$resultado = $sent->execute([
+    'descuento' => $descuento,
+    'cantidad_descuento' => $cantidad_descuento, 'product_id' => $id
+]);
+
 if ($resultado) {
     $_SESSION['exito'] = 'Se ha aplicado correctamente el descuento al producto.';
 } else {
