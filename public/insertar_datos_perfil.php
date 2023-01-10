9060d102c77faed03785543a8ca627ad30b74e6d
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2023-01-10 16:56:00 +0100

    Actualiza insertar_datos_perfil.php

--- public/insertar_datos_perfil.php
+++ public/insertar_datos_perfil.php
@@ -26,29 +26,33 @@ $telefono = obtener_post('telefono');
 if (isset($email) && $email != '') {
     if (!preg_match("/^[A-z0-9\\._-]+@[A-z0-9][A-z0-9-]*(\\.[A-z0-9_-]+)*\\.([A-z]{2,6})$/", $email)) {
         $_SESSION['error'] = "El email es inválido";
-        return (volver_a("/perfil.php"));
     }
 }
 
 if (isset($telefono) && $telefono != '') {
     if (!preg_match("/^\d{9}$/", $telefono)) {
         $_SESSION['error'] = "El teléfono debe contener 9 dígitos y sólo puede contener números";
-        return (volver_a("/perfil.php"));
     }
 }
 
-$sent = $pdo->prepare("UPDATE usuarios
-                        SET nombre = :nombre, apellidos = :apellidos, email = :email, telefono = :telefono
-                        WHERE id = $id");
+try {
+    $sent = $pdo->prepare("UPDATE usuarios
+                            SET nombre = :nombre, apellidos = :apellidos, email = :email, telefono = :telefono
+                            WHERE id = $id");
 
-$sent->execute([
-    ':nombre' => $nombre ?: $origen['nombre'],
-    ':apellidos' => $apellidos  ?: $origen['apellidos'],
-    ':email' => $email ?: $origen['email'],
-    ':telefono' => $telefono ?: $origen['telefono'],
-]);
+    $sent->execute([
+        ':nombre' => $nombre ?: $origen['nombre'],
+        ':apellidos' => $apellidos  ?: $origen['apellidos'],
+        ':email' => $email ?: $origen['email'],
+        ':telefono' => $telefono ?: $origen['telefono'],
+    ]);
 
-$_SESSION['exito'] = 'El perfil del usuario se ha actualizado correctamente.';
+    $_SESSION['exito'] = 'El perfil del usuario se ha actualizado correctamente.';
+
+} catch (Exception $e) {
+
+    $_SESSION['error'] = 'Ha introducido un dato que pertenece a otro usuario.';
+}
 
 
 volver_a("/perfil.php");
